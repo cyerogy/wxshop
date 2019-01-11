5ee8ac4ef472f0525d28d163d1e744a67b2ce869
xiewei <cyerogy@163.com>
2019-01-11 15:03:11 +0800

    文件提交

---
+++ src/pages/order/list.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <van-tabs :active="orderstatus" @change="changeStatus">
+      <van-tab title="全部"></van-tab>
+      <van-tab title="待付款"></van-tab>
+      <van-tab title="待发货"></van-tab>
+      <van-tab title="待收货"></van-tab>
+      <van-tab title="已完成"></van-tab>
+    </van-tabs>
+    <div class="web-font"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      orderstatus: 0
+    }
+  },
+  onLoad() {
+    this.orderstatus = this.$route.query.orderstatus
+  },
+  methods: {
+    changeStatus() {
+
+    }
+  }
+}
+
+</script>
